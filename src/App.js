a8dd4c5682b5c68ed50e9fbcf7c56d21e13ce197
Evgeny <imw1shlol@gmail.com>
2018-09-18 15:36:01 +0300

    Redux added

--- src/App.js
+++ src/App.js
@@ -2,23 +2,83 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import $ from 'jquery';
+import { connect, Provider } from 'react-redux'
+import { createStore } from 'redux'
+import { BrowserRouter, Route } from 'react-router-dom'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Logs />
-      </div>
-    );
+const initialState = {
+  first: "qwer",
+  second: "zxc",
+  items: [],
+  testItems: []
+};
+
+const rootReducer = function (state = initialState, action) {
+  switch (action.type) {
+    case ACTION_CHANGE_FIRST_NAME:
+      return { ...state, first: action.payload }
+    case ACTION_CHANGE_SECOND_NAME:
+      return { ...state, second: action.payload }
+    case ACTION_ADD_TEST_ITEM:
+      const newItems = state.testItems.slice();
+      newItems.push(action.payload);
+      return { ...state, testItems: newItems }
+    case ACTION_UPDATE_LOG_ITEMS:
+      const _items = action.payload.slice();
+      return { ...state, items: _items }
+    default:
+      return state;
   }
-}
+
+};
+const store = createStore(rootReducer);
+//ACTION TYPES
+const ACTION_CHANGE_FIRST_NAME = 'ACTION_CHANGE_FIRST_NAME';
+const ACTION_CHANGE_SECOND_NAME = 'ACTION_CHANGE_SECOND_NAME';
+const ACTION_ADD_TEST_ITEM = 'ACTION_ADD_TEST_ITEM';
+const ACTION_UPDATE_LOG_ITEMS = 'ACTION_UPDATE_LOG_ITEMS';
+
+const mapStateToProps = (state) => {
+  console.log(state);
+  return {
+    first: state.first,
+    second: state.second,
+    items: state.items,
+    testItems: state.testItems
+  };
+};
+
+//ACTIONS
+const ChangeFirst = (newFirst) => {
+  console.log("change first name executed");
+  return {
+    type: ACTION_CHANGE_FIRST_NAME,
+    payload: newFirst
+  }
+};
+const ChangeSecond = (newSecond) => {
+  console.log("change second name executed");
+  return {
+    type: ACTION_CHANGE_SECOND_NAME,
+    payload: newSecond
+  }
+};
+
+const AddTestItem = (newTestItem) => {
+  console.log("add test item executed");
+  return {
+    type: ACTION_ADD_TEST_ITEM,
+    payload: newTestItem
+  }
+};
+
+const UpdateItems = (logItems) => {
+  console.log("update log items executed");
+  return {
+    type: ACTION_UPDATE_LOG_ITEMS,
+    payload: logItems
+  }
+};
 
 class TableHead extends Component {
   render() {
@@ -37,7 +97,22 @@ class TableHead extends Component {
     );
   };
 }
-
+class LogItem extends Component {
+  render() {
+    const argval = this.props.data.ArgType !== "LoanId" ? this.props.data.ArgValue : this.props.data.GuidArgValue;
+    return (
+      <tr>
+        <td>{this.props.data.ActionName}</td>
+        <td>{this.props.data.TimeStampUtc}</td>
+        <td>{this.props.data.UserName}</td>
+        <td>{this.props.data.CompanyName}</td>
+        <td>{this.props.data.UserIp}</td>
+        <td>{this.props.data.ArgType}</td>
+        <td>{argval}</td>
+      </tr>
+    );
+  };
+}
 class Logs extends Component {
   constructor(props) {
     super(props);
@@ -55,12 +130,13 @@ class Logs extends Component {
       dataType: 'json',
       cache: false,
       success: function (data) {
-        this.setState(
+        store.dispatch(UpdateItems(data));
+        /* this.setState(
           {
             LogItems: data,
             isLoaded: true
           }
-        );
+        ); */
         console.log("get from server received!!!");
       }.bind(this),
       error: function (xhr, status, err) {
@@ -72,7 +148,7 @@ class Logs extends Component {
 
 
   render() {
-    const logs = this.state.LogItems;
+    const logs = this.props.items;
     const LogView = logs.map((item) => {
       const logItem = item;
       const time = logItem.TimeStampUtc;
@@ -81,30 +157,70 @@ class Logs extends Component {
       return <LogItem key={item.Id} data={logItem} />
     });
     return (
-      <table className="table table-bordered" >
-        <TableHead />
-        <tbody>
-          {LogView}
-        </tbody>
-      </table>
+      <div>
+        <div className='input-group'>
+          <input type='button' className='form-control'
+            value='Add log'
+            onClick={() => {
+              store.dispatch(AddTestItem("lol"));
+            }}
+          ></input>
+        </div>
+        <div className='input-group'>
+          <input type='text' className='form-control'
+            value={this.props.first}
+            placeholder='first'
+            onChange={(event) => {
+              store.dispatch(ChangeFirst(event.target.value));
+            }}
+          ></input>
+        </div>
+        <div className='input-group'>
+          <input type='text' className='form-control'
+            value={this.props.second}
+            placeholder='second'
+            onChange={(event) => {
+              store.dispatch(ChangeSecond(event.target.value));
+            }}
+          ></input>
+        </div>
+        <table className="table table-bordered" >
+          <TableHead />
+          <tbody>
+            {LogView}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
+const WrappedLogsComponent = connect(mapStateToProps)(Logs);
 
-class LogItem extends Component {
+class SomeComp extends Component {
   render() {
-    const argval = this.props.data.ArgType !== "LoanId" ? this.props.data.ArgValue : this.props.data.GuidArgValue;
     return (
-      <tr>
-        <td>{this.props.data.ActionName}</td>
-        <td>{this.props.data.TimeStampUtc}</td>
-        <td>{this.props.data.UserName}</td>
-        <td>{this.props.data.CompanyName}</td>
-        <td>{this.props.data.UserIp}</td>
-        <td>{this.props.data.ArgType}</td>
-        <td>{argval}</td>
-      </tr>
+      <Provider store={store}>
+        <WrappedLogsComponent />
+      </Provider>
     );
   };
+}
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <BrowserRouter>
+          <Route path='/new' component={SomeComp} />
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 export default App;
